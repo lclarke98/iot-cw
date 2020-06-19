73271d42737933451a30846440ecb2faa37c594c
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-06-19 21:37:12 +0100

    updated

---
+++ cloud-function/cloud-function.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+const pool = mysql.createPool({
+    connectionLimit: 1,
+    socketPath: '/cloudsql/iot-cw:europe-west2:iot',
+    user: '*******',
+    password: '************',
+    database: 'climateSensor'
+});
+exports.handler = function handler(req, res) {
+    // converts message sent from nodeMCU
+    const data = JSON.parse(Buffer.from(req.data, 'base64').toString());
+
+    if (data.includes("temp")) {
+        try {
+            pool.query("INSERT INTO data(humidity, tempC, sensor_ID, room_ID) VALUES (?,?,?,?)", [data.humidity, data.temperature, data.sensorID, data.roomID])
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        try {
+            pool.query("INSERT INTO air(ppm, sensor_ID, room_ID) VALUES (?,?,?,?)", [data.ppm, data.sensorID, data.roomID])
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+};
